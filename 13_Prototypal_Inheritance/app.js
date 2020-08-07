5475e71ad0095917c59259acba5021b74add751b
vishwajeet-29 <48480139+vishwajeet-29@users.noreply.github.com>
2020-08-07 16:31:58 +0530

    Update app.js

--- 13_Prototypal_Inheritance/app.js
+++ 13_Prototypal_Inheritance/app.js
@@ -27,13 +27,17 @@ console.log("Parent Object Value = ",parent.obj.objValue);
 //Now assign properties values for the child
 child.value = "childValue";
 console.log("parent.obj === child.obj => ", parent.value === child.value);
-child.obj = { objValue : "ChildObjectValue"};
+//In case of primitive properties, changing value in child will not reflect in parent
+//In case of objects, thinks behave differently
+changing the object value using child, it also changes the object value of parent
+// child.obj = { objValue : "ChildObjectValue"}; // this will not change the parent object value as here we are creating seperate obj var for child
+child.obj.objValue = "childValue";  //this will change the parent object value to "childValue"
 
 console.log("After changing child properties values")
 console.log("Child Value = ",child.value);
 console.log("Parent Value = ",parent.value);
-console.log("Child Object Value = ",child.obj.objValue);
-console.log("Parent Object Value = ",parent.obj.objValue);
+console.log("Child Object Value = ",child.obj.objValue);            //childValue
+console.log("Parent Object Value = ",parent.obj.objValue);          //childValue
 
 //create grandchild object
 console.log("creating grandChild object");
